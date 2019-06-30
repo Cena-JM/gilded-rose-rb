16cc9b3dd012387e123a2d3787d57e069f50d737
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-06-30 18:05:59 +0300

    add conjured items

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,27 +3,29 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
+          item.quality = if ['Sulfuras', 'Hand of Ragnaros'].include?(item.name)
+                           80
+                         else
+                           if item.name == 'Firestones' # Firestones are Conjured items
+                             item.quality - 2
+                           else
+                             item.quality - 1
+                           end
+                         end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = item.quality + 1 if item.quality < 50
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = item.quality + 1 if item.quality < 50
             end
           end
         end
@@ -35,17 +37,21 @@ class GildedRose
         if item.name != 'Aged Brie'
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
+              item.quality = if ['Sulfuras', 'Hand of Ragnaros'].include?(item.name)
+                               80
+                             else
+                               if item.name == 'Firestones' # Firestones are Conjured items
+                                 item.quality - 2
+                               else
+                                 item.quality - 1
+                               end
+                             end
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality + 1 if item.quality < 50
         end
       end
     end
@@ -61,7 +67,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
